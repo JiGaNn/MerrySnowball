edda133536a5dde5c0d91d2fd611c381a65e00cf
Евгений Афанасьев <evgenij29082002@gmail.com>
2021-12-26 18:18:07 +0800

    создал класс частицы, прописал код в таймере для первоначальной работы с частицами

--- MerrySnowball/Form1.cs
+++ MerrySnowball/Form1.cs
@@ -12,9 +12,61 @@ namespace MerrySnowball
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for(int i = 0; i < 500; i++)
+            {
+                var particle = new Particle();
+
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+
+                particles.Add(particle);
+            }
+        }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rnd.Next(100);
+                    particle.X = picDisplay.Image.Width / 2;
+                    particle.Y = picDisplay.Image.Height / 2;
+                    particle.Direction = Particle.rnd.Next(360);
+                    particle.Speed = 1 + Particle.rnd.Next(10);
+                    particle.Radius = 2 + Particle.rnd.Next(10);
+                }
+                else
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+        }
+        private void Render(Graphics g)
+        {
+            foreach (var part in particles)
+            {
+                part.Draw(g);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                Render(g);
+            }
+            picDisplay.Invalidate();
         }
     }
 }
---
+++ MerrySnowball/Particle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MerrySnowball
+{
+    class Particle
+    {
+        public float X;
+        public float Y;
+        public int Radius;
+        public float Speed;
+        public float Direction;
+        public float Life;
+
+        public static Random rnd = new Random();
+
+        public Particle()
+        {
+            Direction = rnd.Next(360);
+            Speed = 1 + rnd.Next(10);
+            Radius = 2 + rnd.Next(10);
+            Life = 20 + rnd.Next(100);
+        }
+        public void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+
+            int alpha = (int)(k * 255);
+
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
+    }
+}
